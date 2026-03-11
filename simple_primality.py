424c459d3c878566885fdc61f0b98d36ff667553
gisellez143 <giselle114820@mcpsva.org>
2026-03-11 18:00:54 +0000

    Code done

--- simple_primality.py
+++ simple_primality.py
@@ -1,15 +1,22 @@
 """
 Filename: simple_primality.py
-Author: <NAME>
-Created: <DATE>
+Author: <Giselle, Zavala>
+Created: <3/10/2026>
 Instructor: Holtslander
 """
 
 def prime():
     # Write your code here
-
-
-# You should not need to change any code below this point
+    num = input("Enter a non negativew whole number: \n")
+    num= int(num)
+    prime = True
+    for i in range(1, num):
+        if num % i == 0:
+            primew = False
+    if num < 1:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is not a prime number")
 def main():
     print("This program will determine the primality of a number.")
     running = "y"
